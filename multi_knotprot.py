0127034dd156ccee9765968aedc4ad24919e7b5a
Paweł Rubach <pawel.rubach@sorcersoft.com>
2020-01-11 14:09:32 +0100

    Working on multithreading example

--- multi_knotprot.py
+++ multi_knotprot.py
@@ -8,22 +8,26 @@ logger = logging.getLogger(__name__)
 
 types = {'image/png'}
 
-SEARCH_STRING='https://knotprot.cent.uw.edu.pl/browse/?raw=1&set=False&bridgeType=probab%2Ccov_ion&knotTypes=%2B31'
-'https://knotprot.cent.uw.edu.pl/static/knot_data/1j85/A/1j85_A.png'
+SEARCH_STRING = 'https://knotprot.cent.uw.edu.pl/browse/?raw=1&set=False&bridgeType=probab%2Ccov_ion&knotTypes=%2B31'
+DOWNLOAD_LINK = 'https://knotprot.cent.uw.edu.pl/static/knot_data/{0}/{1}/{0}_{1}.png'
 
 
-def get_links(search_string):
+def get_proteins(search_string):
     req = Request(search_string, method='GET')
-
-
+    proteins = []
     with urlopen(req) as resp:
-        data = json.loads(resp.read().decode('utf-8'))
-    return [item['link'] for item in data['data'] if 'type' in item and item['type'] in types]
+        data = resp.read().decode('utf-8')
+        for line in data.splitlines():
+            if len(line)>4 and not line.startswith('#'):
+                cells = line.strip().split(';')
+                proteins.append((cells[0],cells[1]))
+    return proteins
 
 
-def download_link(directory, link):
-    download_path = directory / os.path.basename(link)
-    with urlopen(link) as image, download_path.open('wb') as f:
+def download_link(directory, protein):
+    download_path = os.path.join(directory, os.path.basename('{0}_{1}.png'.format(protein[0],protein[1])))
+    link = DOWNLOAD_LINK.format(protein[0], protein[1])
+    with urlopen(link) as image, open(download_path, 'wb') as f:
         f.write(image.read())
     logger.info('Downloaded %s', link)
 
@@ -33,3 +37,10 @@ def setup_download_dir():
     if not download_dir.exists():
         download_dir.mkdir()
     return download_dir
+
+dir = setup_download_dir()
+proteins = get_proteins(SEARCH_STRING)
+for protein in proteins:
+    download_link(dir, protein)
+
+
