fd343a96718b40eb2e4611d48869517f2bc6ca61
Paweł Rubach <pawel.rubach@sorcersoft.com>
2020-01-11 15:04:04 +0100

    Multithreading examples done

--- multi_knotprot.py
+++ multi_knotprot.py
@@ -1,46 +1,80 @@
-import json
 import logging
-import os
+from concurrent.futures.process import ProcessPoolExecutor
+from functools import partial
 from pathlib import Path
-from urllib.request import urlopen, Request
+from queue import Queue
+from threading import Thread
+from multiprocessing.pool import Pool
 
+from knotprot_download import setup_download_dir, get_proteins, download_link, time_it, create_thumbnail
+
+# Example based by code from:
+# https://www.toptal.com/python/beginners-guide-to-concurrency-and-parallelism-in-python
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-types = {'image/png'}
 
-SEARCH_STRING = 'https://knotprot.cent.uw.edu.pl/browse/?raw=1&set=False&bridgeType=probab%2Ccov_ion&knotTypes=%2B31'
-DOWNLOAD_LINK = 'https://knotprot.cent.uw.edu.pl/static/knot_data/{0}/{1}/{0}_{1}.png'
+# Run in a single Thread
+def run_single():
+    dir = setup_download_dir()
+    proteins = get_proteins()
+    for protein in proteins:
+        download_link(dir, protein)
 
+# Run using multiple Workers
 
-def get_proteins(search_string):
-    req = Request(search_string, method='GET')
-    proteins = []
-    with urlopen(req) as resp:
-        data = resp.read().decode('utf-8')
-        for line in data.splitlines():
-            if len(line)>4 and not line.startswith('#'):
-                cells = line.strip().split(';')
-                proteins.append((cells[0],cells[1]))
-    return proteins
+class DownloadWorker(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
 
-
-def download_link(directory, protein):
-    download_path = os.path.join(directory, os.path.basename('{0}_{1}.png'.format(protein[0],protein[1])))
-    link = DOWNLOAD_LINK.format(protein[0], protein[1])
-    with urlopen(link) as image, open(download_path, 'wb') as f:
-        f.write(image.read())
-    logger.info('Downloaded %s', link)
-
-
-def setup_download_dir():
-    download_dir = Path('images')
-    if not download_dir.exists():
-        download_dir.mkdir()
-    return download_dir
-
-dir = setup_download_dir()
-proteins = get_proteins(SEARCH_STRING)
-for protein in proteins:
-    download_link(dir, protein)
+    def run(self):
+        while True:
+            # Get the work from the queue and expand the tuple
+            directory, link = self.queue.get()
+            try:
+                download_link(directory, link)
+            finally:
+                self.queue.task_done()
 
 
+def workers():
+    dir = setup_download_dir()
+    proteins = get_proteins()
+    queue = Queue()
+    for x in range(4):
+        worker = DownloadWorker(queue)
+        worker.daemon = True
+        worker.start()
+    for protein in proteins:
+            logger.info('Queueing {0}_{1}'.format(protein[0], protein[1]))
+            queue.put((dir,protein))
+    # Causes the main thread to wait for the queue to finish processing all the tasks
+    queue.join()
+
+# End of run using multiple Workers
+
+# Using multiprocessing Pool
+
+def multi_pool():
+    dir = setup_download_dir()
+    proteins = get_proteins()
+    download = partial(download_link, dir)
+    with Pool(8) as p:
+        p.map(download, proteins)
+
+def thumbnails():
+    for image_path in Path('images').iterdir():
+        create_thumbnail((128, 128), image_path)
+
+def thumbnails_pool():
+    thumbnail_128 = partial(create_thumbnail, (128, 128))
+    with ProcessPoolExecutor() as executor:
+        executor.map(thumbnail_128, Path('images').iterdir())
+
+if __name__ == '__main__':
+    #time_it(run_single)
+    #time_it(workers)
+    #time_it(multi_pool)
+    time_it(thumbnails_pool)
